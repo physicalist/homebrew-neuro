59aa3fea3a6e49f092eac30c604b49ca9876d1e4
Jan Pieczkowski <janpi@kth.se>
2014-11-12 15:30:52 +0100

    Fix installation, linking, name conflicts
    - rename convert tool as it conflicted with ImageMagick
    - genesis binary is now copied to #{bin} instead of #{prefix}

--- genesis.rb
+++ genesis.rb
@@ -3,13 +3,18 @@ require "formula"
 class Genesis < Formula
   # description "GENESIS is the GEneral NEural SImulator Suite"
   homepage "http://genesis-sim.org/GENESIS"
-  url "https://github.com/borismarin/genesis2.4gamma/archive/v2.4-RC.tar.gz"
+  url "https://github.com/physicalist/genesis2.4gamma/archive/v2.4-RC.tar.gz"
   sha1 "0f30d729f6c4b47cdac18222ec28d9912da6ca43"
   version "2.4rc"
 
   head "https://github.com/physicalist/genesis2.4gamma.git" #, :using => :git
 
   depends_on :x11 => :recommended
+
+  # Fix:
+  # - GENESIS binary isn't copied to #{bin}
+  # - `convert` tool conflicts with ImageMagick's convert -> rename!
+  patch :DATA
 
   def install
     ENV.deparallelize  # if your formula fails when building in parallel
@@ -39,7 +44,7 @@ class Genesis < Formula
   end
 
   test do
-    system "false" #"#{bin}/genesis"
+    system "#{bin}/genesis"
 
     # `test do` will create, run in and delete a temporary directory.
     #
@@ -52,3 +57,49 @@ class Genesis < Formula
     # executables being tested: `system "#{bin}/program", "do", "something"`.
   end
 end
+__END__
+diff --git a/src/Makefile.BASE b/src/Makefile.BASE
+index b918d57..a9e629f 100644
+--- a/src/Makefile.BASE
++++ b/src/Makefile.BASE
+@@ -114,15 +114,15 @@ copydirs:
+ 
+ install: copydirs
+ 	@(for i in $(FULLDIR); do echo cd $$i; cd $$i; make MACHINE=$(MACHINE) XINCLUDE="$(XINCLUDE)" SPRNG_LIB=$(SPRNG_LIB) DISKIOSUBDIR="$(DISKIOSUBDIR)" INSTALLDIR="$(INSTALLDIR)" INSTALLBIN="$(INSTALLBIN)" RANLIB="$(RANLIB)" install; cd ..;done)
+-	@cp genesis$(EXE_EXT) "$(INSTALLDIR)"
++	@cp genesis$(EXE_EXT) "$(INSTALLDIR)"/bin
+ 	@echo "Done with full install"
+ 
+ nxinstall: copydirs
+ 	@(for i in $(NXDIR); do echo cd $$i; cd $$i; make MACHINE=$(MACHINE) SPRNG_LIB=$(SPRNG_LIB) DISKIOSUBDIR="$(DISKIOSUBDIR)" INSTALLDIR=$(INSTALLDIR) INSTALLBIN=$(INSTALLBIN) RANLIB="$(RANLIB)" install; cd ..;done)
+-	@cp nxgenesis$(EXE_EXT) "$(INSTALLDIR)"
++	@cp nxgenesis$(EXE_EXT) "$(INSTALLDIR)"/bin
+ 	@echo "Done with non-X install"
+ 
+ mininstall: copydirs
+ 	@(for i in $(MINDIR); do echo cd $$i; cd $$i; make MACHINE=$(MACHINE) SPRNG_LIB=$(SPRNG_LIB) INSTALLDIR="$(INSTALLDIR)" INSTALLBIN="$(INSTALLBIN)" RANLIB="$(RANLIB)" install; cd ..;done)
+-	@cp mingenesis$(EXE_EXT) "$(INSTALLDIR)"
++	@cp mingenesis$(EXE_EXT) "$(INSTALLDIR)"/bin
+ 	@echo "Done with minimal install"
+diff --git a/src/convert/Makefile b/src/convert/Makefile
+index a5b4e28..a59e9e5 100644
+--- a/src/convert/Makefile
++++ b/src/convert/Makefile
+@@ -125,14 +125,14 @@ realclean:
+ 	-rm -f *.o y.tab.h y.tab.c lex.yy.c convert
+ 
+ install:
+-	-cp convert$(EXE_EXT) $(INSTALLBIN)
++	-cp convert$(EXE_EXT) $(INSTALLBIN)/genconvert$(EXE_EXT)
+ 	-if test ! -d $(X1COMPAT_DIR); then mkdir $(X1COMPAT_DIR); fi
+ 	-cp X1compat/*.g $(X1COMPAT_DIR)
+ 	-chmod +w $(X1COMPAT_DIR)/*.g
+ 	-if test ! -d $(INSTALLDIR)/man; then mkdir $(INSTALLDIR)/man $(INSTALLDIR)/man/man1; fi
+ 	-if test ! -d $(INSTALLDIR)/man/man1; then mkdir $(INSTALLDIR)/man/man1; fi
+-	-cp convert.man $(INSTALLDIR)/man/man1/convert.1
+-	-cp convert.txt $(INSTALLDIR)/Doc
++	-cp convert.man $(INSTALLDIR)/man/man1/genconvert.1
++	-cp convert.txt $(INSTALLDIR)/Doc/genconvert.txt
+ 
+ freeze:
+ 	rcsclean
