8b48e3b7a92f1d5410e9863ed8c261be64d19fc8
Jan Pieczkowski <janpi@kth.se>
2014-11-12 17:46:06 +0100

    Version bump to v2.4-rc2 (installfix)

--- genesis.rb
+++ genesis.rb
@@ -3,9 +3,9 @@ require "formula"
 class Genesis < Formula
   # description "GENESIS is the GEneral NEural SImulator Suite"
   homepage "http://genesis-sim.org/GENESIS"
-  url "https://github.com/physicalist/genesis2.4gamma/archive/v2.4-RC.tar.gz"
-  sha1 "0f30d729f6c4b47cdac18222ec28d9912da6ca43"
-  version "2.4rc"
+  url "https://github.com/physicalist/genesis2.4gamma/archive/v2.4-rc2.tar.gz"
+  sha1 "1e28893a22436e333d3917a5fc35dc346475bd08"
+  version "2.4-rc2"
 
   head "https://github.com/physicalist/genesis2.4gamma.git" #, :using => :git
 
@@ -39,22 +39,10 @@ class Genesis < Formula
       system "make", "nxgenesis"
       system "make", "nxinstall"
     end
-
-    #system "make", "install INSTALLDIR=#{prefix}/genesis/#{version}"
   end
 
   test do
     system "#{bin}/genesis"
-
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test genesis2.4gamma`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
   end
 end
 __END__
